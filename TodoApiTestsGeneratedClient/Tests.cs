3629d2d0c6592491e7040cbe0ef6d9edbc39d56f
Markus Thulin <macky_b@hotmail.com>
2023-03-11 11:05:55 +0100

    Created new test project in solution

---
+++ TodoApiTestsGeneratedClient/Tests.cs
@@ -0,0 +1,16 @@
+namespace TodoApiTestsGeneratedClient
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
